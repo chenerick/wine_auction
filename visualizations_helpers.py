8e22086c7e7b85f6801361f3f3983e71f65d781c
chenerick <chenerick@gmail.com>
2018-12-02 18:59:00 -0500

    Refactor 1) including raw data dump as csv 2) Price change over time

--- visualizations_helpers.py
+++ visualizations_helpers.py
@@ -3,6 +3,7 @@ import warnings
 warnings.filterwarnings("ignore")
 from scipy import stats
 import datetime as dt
+import pandas as pd
 
 date_col = 'Date'
 vintage_col = 'Vintage'
@@ -41,3 +42,32 @@ def regress_bottle_price_time(bottle_df):
     #print(slope, intercept, r_value, p_value, std_err)
     #print(marginal_dollar_per_year, first_price, annualized_price_change)
     return annualized_price_change
+
+'''
+Given a bottle_df, for each unique_bottle_key computes the normalized price percent change
+Enriches each row of bottle_df with the percent change for that bottle key
+Removes all bottles of w/ fewer than count_threshold observations
+'''
+def enrich_bottle_price_percent_change(bottle_df, count_threshold = 25):
+    # Start w/ the bottle dataframe - all lots of just type. UnitPriceUSD represents the per bottle price
+    # Treat each lot as an individual observation. Get the slope of Price(Time). Slope will represent the marginal change in price per marginal change in time
+    bottle_grouped = bottle_df.groupby(unique_bottle_keys).UnitPriceUSD.agg(['min', 'mean', 'max', "count"])
+    # bottle_df.head()
+    bottle_grouped = bottle_grouped.sort_values('count', ascending=False)
+    # bottle_grouped.head()
+    # Join the ungrouped df with the grouped attributes.
+    bottle_df_with_grouped_attr = pd.merge(bottle_df, bottle_grouped.reset_index(), on=unique_bottle_keys)
+    bottle_df_with_grouped_attr = bottle_df_with_grouped_attr.sort_values('count', ascending=False)
+
+    # Only keep bottles that appear in more than count_threshold lots
+    count_col = 'count'
+    high_count_bottles = bottle_df_with_grouped_attr[bottle_df_with_grouped_attr[count_col] >= count_threshold]
+    # show high count bottles w/ annual percent change
+    # Compute the normalized slope for each high count bottle and stitch together w/ grouped attributes
+    bottle_grouped_percent_change = high_count_bottles.groupby(unique_bottle_keys).apply(
+        lambda x: pd.Series({annual_percent_change_col: regress_bottle_price_time(x)}))
+    bottle_grouped_percent_change = pd.merge(bottle_grouped.reset_index(), bottle_grouped_percent_change.reset_index(),
+                                             on=unique_bottle_keys)
+    bottle_grouped_percent_change = bottle_grouped_percent_change.sort_values(annual_percent_change_col, ascending=False)
+
+    return bottle_grouped_percent_change
